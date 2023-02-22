80b3d3ba1c3c3411cf2e4553f1e2400b97030940
KhadijahAbeni <126070190+KhadijahAbeni@users.noreply.github.com>
2023-02-22 02:59:44 -0800

    Add files via upload

---
+++ python day1.py
@@ -0,0 +1,17 @@
+word = 'MY NAME IS JAMES'
+print(word)
+print ('\n')
+state = 'i\'m from a well known village with good culture in kogi state'
+print(state)
+print ('\n')
+totalmoney1 = 1000
+quantity2 = 3
+price3 = 450
+report = 'i have {} dollars so i can buy {} football for {} dollars'
+print(report.format(totalmoney1,quantity2,price3))
+peom = '''twinkle twinkle little star
+how i wonder what you are
+up above the sky so high
+like a diamong in the sky
+'''
+print(peom)
